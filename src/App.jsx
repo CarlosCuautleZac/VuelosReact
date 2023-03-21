ced88284b6fee4b4467c6ef314a28915955891dd
Genaro-MC <181G05123@rcarbonifera.tecnm.mx>
2023-03-21 17:51:34 -0600

    Agregado de fetch en main

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+const [data, setData] = useState(null);
+
+useEffect(()=>{
+  fetch("https://aerolineatec.sistemas19.com/api/vuelos").then((response)=>response.json()).then((data)=>setData(data));
+},[]);
 
   return (
-    <main>
-      <div>
-
+    
+      <main>
+        <div>
+         <h1>Vuelos</h1> 
+         <div>
+          {data?.map((vuelo)=>(<h2 key={vuelo.id}>{vuelo.destino}</h2>))}
+         </div>
       </div>
-    </main>
+      </main>
+ 
   )
 }
 
