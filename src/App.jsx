7e54e1594158c4fcf20316610bf2448d8e321f77
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2023-03-29 16:58:58 -0600

    Formato a los datos

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,41 @@ function App() {
 const {data, loading, error} = useFetch("https://aerolineatec.sistemas19.com/api/vuelos");
 
   return (
+   
     
-      <main>
-        <div>
-         <h1>Vuelos</h1> 
-         <div>
-          <ul>
-            {error && <li>Error:{error}</li>}
-            {loading&&<li>Loading...</li>}
-            {data?.map((vuelo)=>( <li><h2 key={vuelo.id}>{vuelo.destino}</h2></li> ))}
-          </ul>
-         </div>
-      </div>
-      </main>
- 
+    <div class="container">
+    <h2 className='text-center pb-2 mb-2'>Departures</h2>
+    <div class="table-responsive">
+        <table class="table table-dark table-hover">
+            <thead>
+                <tr>
+                    <th>Flight No.</th>
+                    <th>Destination</th>
+                    <th>Time</th>
+                    <th>Gate</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+
+
+                    {error && <li>Error:{error}</li>}
+                    {loading&&<li>Loading...</li>}
+                    {data?.map((vuelo)=>( 
+                    <tr key={vuelo.id}>
+                        <td data-label="Flight No.">{vuelo.numerovuelo}</td>
+                       <td data-label="Destination" >{vuelo.destino}</td>
+                       <td data-label="Time" >{new Date(vuelo.fecha).toLocaleString('es-ES', 
+                       {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'})}</td>
+                      <td data-label="Gate">{vuelo.puerta}</td>
+                      <td data-label="Status" class={vuelo.estado === 'Programado' ? 'green' : vuelo.estado === 'Cancelado' ? 'red' : ''}>{vuelo.estado}</td>
+                       </tr> ))}
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
   )
 }
 
