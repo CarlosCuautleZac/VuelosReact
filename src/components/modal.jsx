74d547bb74be3e773b326a7b12455f3ad3b0115a
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2023-04-15 09:03:31 -0600

    Post Terminado

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,17 +1,59 @@
 import React, { useState } from "react";
 import "./Modal.css";
+// import { PostModule } from "../scripts/PostModule";
 
 export function Modal() {
   const [modal, setModal] = useState(false);
+  const [succes,setSucces] = useState(false);
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+
+    const url = "https://aerolineatec.sistemas19.com/api/vuelos";
+    const botonEnviar = document.querySelector("#enviar");
+    botonEnviar.addEventListener("click", (event) => {
+      //console.log('Le diste clic');
+      var data = new FormData(document.querySelector("#formVuelo"));
+      var jsonData = {};
+      data.forEach((value, key) => (jsonData[key] = value));
+      var json = JSON.stringify(jsonData);
+      console.log(json);
+      fetch(url, {
+        body: json,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+        .then(async (response) =>{
+          const status = await response;
+
+           console.log(response)
+        if(status.ok)
+          {
+            setModal(!modal);
+            alert("¡Enviado con exito!")
+          }
+            else{
+              const text = await response.text();
+              throw Error(text);      
+            }        
+          }
+        )       
+        .catch((error) => {console.log(error)
+        alert(error)});
+    });
+  };
 
   const toggleModal = () => {
     setModal(!modal);
   };
 
-  if(modal) {
-    document.body.classList.add('active-modal')
+  if (modal) {
+    document.body.classList.add("active-modal");
   } else {
-    document.body.classList.remove('active-modal')
+    document.body.classList.remove("active-modal");
   }
 
   return (
@@ -24,26 +66,38 @@ export function Modal() {
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
-            <h2 className="color">Hello Modal</h2>
-            <form id="formVuelo">
-<label>Destino</label>
-<input type="text" name="destino"/><br/>
-<label>Num. vuelo</label>
-<input type="text" name="numerovuelo"/><br/>
-<label>Puerta</label>
-<input type="text" name="puerta"/><br/>
-<label>Fecha</label>
-<input type="datetime-local" name="fecha"/><br/>
-<input type="hidden" value="Programado" name="estado"/>
-<input type="button" id="enviar" value="Enviar"/>
-</form>    
+            <h2 className="color">Nuevo Vuelo</h2>
+            <form  id="formVuelo" className="form-vuelo">
+              <label>Destino</label>
+              <input type="text" name="destino" />
+              <br />
+              <label>Num. vuelo</label>
+              <input type="text" name="numerovuelo" />
+              <br />
+              <label>Puerta</label>
+              <input type="text" name="puerta" />
+              <br />
+              <label>Fecha</label>
+              <input type="datetime-local" name="fecha" />
+              <br />
+              <input type="hidden" value="Programado" name="estado" />
+              {/* <input type="button" id="enviar" onClick={toggleModal} value="Enviar" /> */}
+              <button
+                type="submit"
+                id="enviar"
+                onClick={handleSumbit}
+                value="Enviar"
+              >
+                Enviar
+              </button>
+            </form>
 
-            <button className="close-modal" onClick={toggleModal}>
+            {/* <button className="close-modal" onClick={toggleModal}>
               CLOSE
-            </button>
+            </button> */}
           </div>
         </div>
-      )}     
+      )}
     </>
   );
 }
